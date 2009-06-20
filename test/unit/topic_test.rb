a76e11ff73505cf52ffa4a3c043ac46030b9591d
Sarah Allen <sarah@ultrasaurus.com>
2009-06-20 11:14:02 -0700

    generate vote resources, model tests for associations and validations

--- test/unit/topic_test.rb
+++ test/unit/topic_test.rb
@@ -1,8 +1,19 @@
 require 'test_helper'
 
 class TopicTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "Require title to not be blank" do
+    t = Topic.new
+    assert t.valid? == false, "Expected Topic to not be valid"
+  end
+
+  test "Topic with title is valid" do
+    t = Topic.new(:title => "my title")
+    assert t.valid?, "Topic with title should be valid"
+  end
+
+  test "Topic has votes" do
+    t = Topic.new(:title => "my title")
+    assert t.votes == []
   end
 end
