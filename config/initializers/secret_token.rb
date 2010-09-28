fc24b087b505a84ffe447d2705306e2d129b4d2e
Sarah Mei <pair+sarah@pivotallabs.com>
2010-09-28 13:22:29 -0700

    Empty Rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsbridgeWorkshop::Application.config.secret_token = '316209b085883021ee261edfd68e68ba2ea6632d75b4f3b6dbcd216d8c24cb91d1343c08c4d98531c37ba508224c67517a42d3390925e63c6697c282b20d6263'
